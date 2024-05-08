be81a677a928c2c485036486057e328aa62be527
ProudDesk <timherdemark86@gmail.com>
2024-05-08 16:46:47 +0200

    chore: Remove duplicate code and unnecessary header file imports in calendar.c

--- calendar.c
+++ calendar.c
@@ -19,16 +19,8 @@ void clearScreen() {
 #endif
 }
 
-#include <stdio.h>
-
 #define GREEN "\033[0;32m"
 #define RESET "\033[0m"
-
-#include <stdio.h>
-
-#define GREEN "\033[0;32m"
-#define RESET "\033[0m"
-
 void displayMonth(const char *month, int days, int currentDay, int startDay) {
     // Top border
     printf("+-----------------------------------------+\n");
